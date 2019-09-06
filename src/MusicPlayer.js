78ba692e562702e9e5931423b01418d0a4159b3a
Lawrence <lawrencema415@gmail.com>
2019-09-06 13:48:43 -0700

    adding end song

--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -8,13 +8,14 @@ class MusicPlayer extends Component {
       songUrl:"https://spotify-clone.s3-us-west-1.amazonaws.com/Ozuna+-+Aura/14.+Unica.mp3",
       isPlaying:false,
       progress:0,
-      dragProgressBar:false
+      dragProgressBar:false,
+      loop:false
     }
     this.progressInChange =  false;
-    this.interval = setInterval(this.updateProgress,250);
+    this.interval = setInterval(this.update,1000);
   }
 
-  updateProgress = () => {
+  update = () => {
     let player = this.refs.player;
     if(this.refs.player) {
       if(!this.progressInChange) {
@@ -22,6 +23,9 @@ class MusicPlayer extends Component {
           progress: player.currentTime / player.duration
         });
       }
+    }
+    if(player.ended) {
+      console.log("done");
     }
   }
 
@@ -48,19 +52,20 @@ class MusicPlayer extends Component {
       let progress = ((e.clientX - offsetLeftConvert(this.refs.progress_bar)) / this.refs.progress_bar.clientWidth);
       console.log(progress);
       this.setState({progress:progress});
-      this.progressInChange = false
+      this.progressInChange = true
     }
   }
 
   render() {
-    let currenTime = 0;
+    let currentTime = 0;
     let totalTime = 0;
     if(this.refs.player) {
       let player = this.refs.player;
+      player.loop = this.state.loop;
       if(player.currentSrc !== this.state.songUrl) {
         player.src = this.state.songUrl;
       }
-      if(player.paused) {
+      if(player.paused && !player.ended) {
         if(this.state.isPlaying) {
           player.play();
         }
@@ -71,6 +76,9 @@ class MusicPlayer extends Component {
         this.progressInChange = false;
         player.currentTime = player.duration * this.state.progress;
       }
+
+      currentTime = player.currentTime;
+      totalTime = player.duration;
 
     }
 
@@ -86,13 +94,13 @@ class MusicPlayer extends Component {
           <a onClick={this.togglePlay}><i className={classnames(playerClassName)} aria-hidden="true"></i></a>
           <a href="#"><i className="fa fa-chevron-right" aria-hidden="true"></i></a>
         </div>
-        <div className="progress" onMouseDown={this.progressBarActivate} onClick={this.setTime} onMouseMove={this.setTime} onMouseLeave={() => this.setState({dragProgressBar:false})} onMouseUp={this.stopDrag}>
+        <div className="progress" onMouseDown={this.progressBarActivate} onMouseMove={this.setTime} onMouseLeave={() => this.setState({dragProgressBar:false})} onMouseUp={this.stopDrag}>
           <div ref="progress_bar" className="bar">
           <div style={{width: (this.state.progress * 100) + "%" }}></div>
           </div>
         </div>
-
-        <audio ref="player" autoPlay={this.state.isPlaying}>
+        <div className="timeStamp"> {convertTime(currentTime)} / {convertTime(totalTime)} </div>
+        <audio ref="player" autoplay={this.state.isPlaying}>
           <source src={this.state.songUrl}/>
         </audio>
 
@@ -100,6 +108,13 @@ class MusicPlayer extends Component {
     );
   }
 };
+
+function convertTime(time) {
+  let minutes = Math.floor(time % 3600 / 60);
+  let seconds = Math.floor(time % 3600 % 60);
+
+  return minutes + ":" + seconds;
+}
 
 function offsetLeftConvert(el) {
   let left = 0;
